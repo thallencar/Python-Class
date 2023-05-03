99e1722366d1b52950d750edda6eff5b15785934
Thalita Alencar <125590093+thallencar@users.noreply.github.com>
2023-05-03 02:05:15 -0300

    Iniciação Laços

---
+++ Laços, 10-04/ex01.py
@@ -0,0 +1,13 @@
+"""ini = float(input("Digite um valor: "))
+i = 0
+while i = 0:
+    i =+ 1
+print("i")"""
+
+vi = int(input("Digite o valor inicial: "))
+vf = int(input("Digite o valor final: "))
+
+
+while vi <= vf:
+    print(vi)
+    vi = vi + 1
---
+++ Laços, 10-04/parar-negativo.py
@@ -0,0 +1,9 @@
+"""soma = 0
+num = 1
+while num != 0:
+    num = float(input("Digite um número: "))
+    soma = soma + num
+print ("Somatória = ", soma)"""
+1 == "Huguinho""
+
+print (1)
