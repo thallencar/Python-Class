d505823431a9d54e06818f8b96276bc4a366c20f
Thalita Alencar <125590093+thallencar@users.noreply.github.com>
2023-05-03 01:58:53 -0300

    Delete Exercício 01.py

--- Exercício 01.py
+++
@@ -1,8 +0,0 @@
-"""""
-EXERCÍCIO 01:
-Dado uma venda pelo usuário, calcular 12% de desconto caso a venda seja maior do que 500.
-"""
-n = float(input("Digite aqui o total da compra: "))
-if n > 500:
-    n = n*0.88
-print("Com o desconto, o valor total de sua compra foi de: ", n)
